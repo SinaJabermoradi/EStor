06f10c4941f785685bcc4e60a1ad171f3d7855e6
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-11-27 00:30:59 +0330

    - Add Data Base Context In Persistence Layer

--- EStor.Persistence/Context/DataBaseContext.cs
+++ EStor.Persistence/Context/DataBaseContext.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EStor.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace EStor.Persistence.Context
 {
     public class DataBaseContext : DbContext
     {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        #region DbSet تبدیل کردن کلاس های مدلمون یا انتیتی مون به جداول دیتابیس به کمک 
+        
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserRole> Roles { get; set; }
+
+        public DbSet<UserInRole> UserInRoles { get; set; }
+
+        #endregion
 
     }
 }
