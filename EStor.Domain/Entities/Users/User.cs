da22ec7feef27a725807d060005add4828eca625
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-11-26 20:09:28 +0330

    - Add Entities ===> Users

---
+++ EStor.Domain/Entities/Users/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStor.Domain.Entities.Users
+{
+    public class User
+    {
+        public long Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        /// <summary>
+        /// برای ایجاد رابطه ی چند به چند از این پراپرتی و کلاس (( یوزر این رول )) استفاده کردیم
+        /// </summary>
+        public ICollection<UserInRole> UserInRoles { get; set; }
+    }
+}
---
+++ EStor.Domain/Entities/Users/UserRole.cs
@@ -0,0 +1,14 @@
+﻿namespace EStor.Domain.Entities.Users;
+
+public class UserRole
+{
+    public long Id { get; set; }
+
+    public Roles Role { get; set; }
+
+    /// <summary>
+    /// برای ایجاد رابطه ی چند به چند از این پراپرتی و کلاس (( یوزر این رول )) استفاده کردیم
+    /// </summary>
+    public ICollection<UserInRole> UserInRoles { get; set; }
+
+}
