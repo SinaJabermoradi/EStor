a9e840066bd1ee4648a5671d5844870116ed999a
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-11-26 11:39:40 +0330

    -Add Admin Template Css & JS & images & Fonts -Add Admin Template Html Code

--- EndPoint.MVCWebApplicationUI/Program.cs
+++ EndPoint.MVCWebApplicationUI/Program.cs
@@ -26,9 +26,19 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
+
 
 app.Run();
 
