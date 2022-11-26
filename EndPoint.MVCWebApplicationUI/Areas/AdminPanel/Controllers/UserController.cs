50efacb9c12e1c231e7a4f7c109c3066781a981e
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-11-26 16:11:44 +0330

    -Add WebApplication Template

---
+++ EndPoint.MVCWebApplicationUI/Areas/AdminPanel/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.MVCWebApplicationUI.Areas.AdminPanel.Controllers
+{
+    [Controller]
+    public class UserController : Controller
+    {
+        [Area("AdminPanel")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
