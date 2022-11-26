6367ccb0b6040b85a1f8df6bb4f0514961a3d658
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-11-27 00:30:13 +0330

    -Add DataBaseContext Interface In Application Layer

---
+++ EStor.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EStor.Domain.Entities.Users;
+
+namespace EStor.Application.Interfaces.Contexts
+{
+    public interface IDataBaseContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserRole> Roles { get; set; }
+
+        public DbSet<UserInRole> UserInRoles { get; set; }
+    }
+}
