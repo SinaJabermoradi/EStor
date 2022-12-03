a19a56305bcd30d66b5815ebbca90837e21593a2
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-03 04:14:24 +0330

    - Add RemoveUser

---
+++ EStor.Application/Services/Users/CommandsService/RemoveUser/RemoveUserService.cs
@@ -0,0 +1,35 @@
+﻿using EStor.Application.Interfaces.Contexts;
+using EStor.CommonUtility.DTO;
+
+namespace EStor.Application.Services.Users.CommandsService.RemoveUser;
+
+public class RemoveUserService : IRemoveUserService
+{
+    private readonly IDataBaseContext _context;
+
+    public RemoveUserService(IDataBaseContext context)
+    {
+        _context = context; 
+    }
+
+    public ServiceResultDto Execute(long userId)
+    {
+        var user = _context.Users.Find(userId);
+        if (user == null)
+        {
+            return new ServiceResultDto
+            {
+                IsSuccess = false,
+                Message = "کاربر یافت نشد"
+            };
+        }
+        user.RemoveRecordTime = DateTime.Now;
+        user.IsRemoved = true;
+        _context.SaveChanges();
+        return new ServiceResultDto
+        {
+            IsSuccess = true,
+            Message = "کاربر با موفقیت حذف شد"
+        };
+    }
+}
