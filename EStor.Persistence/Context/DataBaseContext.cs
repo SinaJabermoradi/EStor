47c9fdbe91f608ff40a3ccb67f56c944912df9b2
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-03 04:11:57 +0330

    - Update Data Base Context

--- EStor.Persistence/Context/DataBaseContext.cs
+++ EStor.Persistence/Context/DataBaseContext.cs
@@ -11,27 +11,62 @@ namespace EStor.Persistence.Context
 
         }
 
-        #region DbSet تبدیل کردن کلاس های مدلمون یا انتیتی مون به جداول دیتابیس به کمک 
+        #region DbSet تبدیل کردن کلاس های انتیتی مون به جداول دیتابیس به کمک 
         
         public DbSet<User> Users { get; set; }
 
-        public DbSet<UserRole> Roles { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
-        public DbSet<UserInRole> UserInRoles { get; set; }
+        public DbSet<UserRole> UsersRoles { get; set; }
 
         #endregion
 
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess
+                                , CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<UserRole>().HasData(new UserRole 
+
+            #region افزودن ِمقادیر پیشفرض ِنقش ها در دیتابیس
+
+            modelBuilder.Entity<Role>().HasData(new Role
                 { Id = 1, Name = EStor.CommonUtility.Roles.Roles.Admin });
-           
-            modelBuilder.Entity<UserRole>().HasData(new UserRole
+
+            modelBuilder.Entity<Role>().HasData(new Role
                 { Id = 2, Name = EStor.CommonUtility.Roles.Roles.Operator });
 
-            modelBuilder.Entity<UserRole>().HasData(new UserRole
-                {
-                    Id = 3 , Name =  EStor.CommonUtility.Roles.Roles.Customer});
+            modelBuilder.Entity<Role>().HasData(new Role
+            {
+                Id = 3,
+                Name = EStor.CommonUtility.Roles.Roles.Customer
+            });
+
+            #endregion
+
+            modelBuilder.Entity<User>().HasIndex(user => user.Email).IsUnique();
+
+
+            modelBuilder.Entity<User>().HasQueryFilter(user=> !user.IsRemoved); // به کمک این قطعه کد مشخص می کنیم که انتیتی یوزر ، فقط اون یوزر ها یا کاربرانی رو برای من لود کن که (( ایز ریموو )) نباشن
+
         }
     }
 }
