9924c248418de95a2c6853b4026794d7f71e49aa
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-03 04:16:10 +0330

    - Add Request Edit User

---
+++ EStor.Application/Services/Users/CommandsService/EditUser/RequestEditUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace EStor.Application.Services.Users.CommandsService.EditUser;
+
+public class RequestEditUserDto
+{
+    public long UserId { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string RePassword { get; set; }
+}
