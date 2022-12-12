b39abfcf45b9460c9ae553bc49c4c0cb5ce34fac
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-12 23:39:54 +0330

    - Update Product ===> - Add IPoduct Facad

---
+++ EStor.Application/Interfaces/FacadPattern/IProductFacad.cs
@@ -0,0 +1,15 @@
+﻿using EStor.Application.Services.Products.CommandsService.AddNewCategory;
+using EStor.Application.Services.Products.CommandsService.AddNewProduct;
+using EStor.Application.Services.Products.QueriesService.GetAllCategories;
+using EStor.Application.Services.Products.QueriesService.GetCategories;
+
+namespace EStor.Application.Services.Products.FacadPattern;
+
+public interface IProductFacad
+{
+    public AddNewCategoryService AddNewCategoryService { get; }
+    public IGetCategoriesService GetCategoriesService { get;}
+    public AddNewProductService AddNewProductService { get; }
+    public IGetAllCategoriesService GetAllCategoriesService { get; }
+
+}
