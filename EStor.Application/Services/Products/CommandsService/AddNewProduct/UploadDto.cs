853f16e4640b0dad019b564b43942a654ef9d439
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-12 23:41:38 +0330

    - Add Upload Dto

---
+++ EStor.Application/Services/Products/CommandsService/AddNewProduct/UploadDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EStor.Application.Services.Products.CommandsService.AddNewProduct;
+
+public class UploadDto
+{
+    public long Id { get; set; }
+    public bool IsHaveStatus { get; set; }
+    public string FileNameAddress { get; set; }
+}
