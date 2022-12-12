c087d60b585b070b20c640bd4e0a0a6213aa856f
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-12 23:37:18 +0330

    - Add Category File

---
+++ EndPoint.MVCWebApplicationUI/Areas/AdminPanel/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using EStor.Application.Services.Products.FacadPattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.MVCWebApplicationUI.Areas.AdminPanel.Controllers
+{
+    [Controller]
+    [Area("AdminPanel")]
+    public class CategoriesController : Controller //  دسته بندی محصولات
+    {
+        private readonly IProductFacad _productFacad;
+
+        public CategoriesController(IProductFacad productFacad)
+        {
+            _productFacad = productFacad;
+        }
+
+        [HttpGet]
+        public IActionResult Index(long? parentId)
+        {
+            return View(_productFacad.GetCategoriesService.Execute(parentId).Data);
+        }
+
+        [HttpGet]
+        public IActionResult AddNewCategory(long? parentId)
+        {
+            ViewBag.ParentId = parentId;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNewCategory(long? parentId
+                        , string name)
+        {
+            var addNewCategoryResult = _productFacad.AddNewCategoryService.Execute(parentId, name);
+            return Json(addNewCategoryResult);
+        }
+    }
+}
