cc51a8108a1e748010a998935cd3d063916509bd
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-19 14:54:42 +0330

    - Add GetMenu View Component

---
+++ EndPoint.MVCWebApplicationUI/ViewComponents/GetMenu.cs
@@ -0,0 +1,23 @@
+﻿using EStor.Application.Services.Common.QueriesService.GetMenuItem;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EndPoint.MVCWebApplicationUI.ViewComponents
+{
+    public class GetMenu : ViewComponent
+    {
+        private readonly IGetMenuItemService _menuItemService;
+        public GetMenu(IGetMenuItemService menuItemService)
+        {
+            _menuItemService = menuItemService;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            return View(viewName: "GetMenu", model: _menuItemService.Execute().Data);
+        }
+    }
+}
+
+
