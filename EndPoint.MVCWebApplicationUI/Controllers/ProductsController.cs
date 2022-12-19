b310903c684c21d8cb4f4c0e05cd433b5bf06279
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-19 14:56:10 +0330

    - Add Product Controller For Site

---
+++ EndPoint.MVCWebApplicationUI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using EStor.Application.Services.Products.FacadPattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.MVCWebApplicationUI.Controllers
+{
+    [Controller]
+    public class ProductsController : Controller
+    {
+        private readonly IProductFacad _productFacad;
+        public ProductsController(IProductFacad productFacad)
+        {
+            _productFacad = productFacad;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index(string searchKey
+                        , long? categoryId
+                        , int page)
+        {
+            return View(_productFacad.GetProductForSite.Execute(searchKey,page , categoryId).Data);
+        }
+
+
+        [HttpGet]
+        public IActionResult Detail([FromQuery]long productId)
+        {
+            return View(_productFacad.GetProductDetailForSite.Execute(productId).Data);
+        }
+
+    }
+}
