479f21caecaee8be48054f7e0b6d0773f263f5cb
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-19 14:56:52 +0330

    - Add Edit Product View Model For Admin

---
+++ EndPoint.MVCWebApplicationUI/Areas/AdminPanel/Models/EditProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using EStor.Application.Services.Products.CommandsService.AddNewProduct;
+
+namespace EndPoint.MVCWebApplicationUI.Areas.AdminPanel.Models
+{
+    public class EditProductViewModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public long CountOfProductInInventory { get; set; }
+        public bool IsThisProductBeDisplayedOnTheSite { get; set; }
+        public long CategoryId { get; set; }
+        public List<IFormFile> Images { get; set; }
+        public List<AddNewProduct_Features> Features { get; set; }
+    }
+}
