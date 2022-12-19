40385fc2098cc835a654b97eed7d94c9237446f9
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-19 14:54:29 +0330

    - Add Search View Component

---
+++ EndPoint.MVCWebApplicationUI/ViewComponents/Search.cs
@@ -0,0 +1,19 @@
+﻿using EStor.Application.Services.Common.QueriesService.GetCategoryForSearch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EndPoint.MVCWebApplicationUI.ViewComponents
+{
+    public class Search : ViewComponent
+    {
+        private readonly IGetCategoryForSearchService _getCategoryForSearch;
+        public Search(IGetCategoryForSearchService getCategoryForSearch)
+        {
+            _getCategoryForSearch = getCategoryForSearch;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(viewName: "Search", model: _getCategoryForSearch.Execute().Data);
+        }
+    }
+}
