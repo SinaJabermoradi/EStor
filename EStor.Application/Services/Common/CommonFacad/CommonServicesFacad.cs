ec1bdd8a54f9c24eaf42c134b331af2b4f676c8d
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-19 14:43:28 +0330

    - Add Common Facad Service

---
+++ EStor.Application/Services/Common/CommonFacad/CommonServicesFacad.cs
@@ -0,0 +1,25 @@
+﻿using EStor.Application.Interfaces.Contexts;
+using EStor.Application.Services.Common.QueriesService.GetMenuItem;
+using EStor.Application.Services.Products.FacadPattern;
+
+namespace EStor.Application.Services.Common.CommonFacad;
+
+public class CommonServicesFacad : ICommonServicesFacad
+{
+    private readonly IDataBaseContext _context;
+    public CommonServicesFacad(IDataBaseContext context)
+    {
+        _context = context;
+    }
+
+
+
+    private IGetMenuItemService _getMenuItemService;
+    public IGetMenuItemService GetMenuItemService
+    {
+        get
+        {
+            return _getMenuItemService = _getMenuItemService ?? new GetMenuItemService(_context);
+        }
+    }
+}
