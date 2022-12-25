b8a926816f6527d376c92c193ebea6cc31accb19
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-25 11:47:43 +0330

    - Update DataBase Context ===> - Add Slider - Add HomePageImage

--- EStor.Application/Interfaces/Contexts/IDataBaseContext.cs
+++ EStor.Application/Interfaces/Contexts/IDataBaseContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using EStor.Domain.Entities.HomePage;
 using EStor.Domain.Entities.Products;
 using EStor.Domain.Entities.Users;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,10 @@ namespace EStor.Application.Interfaces.Contexts
         public DbSet<ProductImage> ProductImages { get; set; }
 
         public DbSet<ProductFeatures> ProductFeatures { get; set; }
+
+        public DbSet<Slider> Slider { get; set; }
+
+        public DbSet<HomePageImage> HomePageImages { get; set; }
 
         public int SaveChanges();
 
