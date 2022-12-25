adaf6382b1637134db74d8a1572deb9c27900be8
مهندس سینا جابرمرادی <sinajabermoradi313@gmail.com>
2022-12-25 11:41:04 +0330

    - Add Slider Entity

---
+++ EStor.Domain/Entities/HomePage/Slider.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+using EStor.Domain.Entities.Commons;
+
+namespace EStor.Domain.Entities.HomePage;
+
+public class Slider : BaseEntity
+{
+    public string ImageSrc { get; set; }
+    public string Link { get; set; }  // اگر روی عکس کلیک شد به کجا بره
+    public long ClickCount { get; set; }  // تعداد دفعاتی که کاربران روی این عکس کلیک کردن ===> کلیک خورش چقدره
+}
